1f484281eab25d9f5df5072595dad7e9669be4c1
Mohamed El Kanboui Bouaacha <mohamedelkanboui10@gmail.com>
2025-10-29 13:09:27 +0100

    Test de integración con la API FacturaDirecta

--- WebControlGH/src/index.js
+++ WebControlGH/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react'; // Importa React
-import ReactDOM from 'react-dom/client'; // Importa ReactDOM para renderizar la aplicación
-import './css/index.css'; // Importa los estilos globales
-import App from './components/App/App'; // Importa el componente App
-import reportWebVitals from './reportWebVitals'; // Importa la función para reportar métricas de rendimiento
+import React from "react"; // Importa React
+import ReactDOM from "react-dom/client"; // Importa ReactDOM para renderizar la aplicación
+import "./css/index.css"; // Importa los estilos globales
+import App from "./Frontend/App/App"; // Importa el componente App
+import reportWebVitals from "./reportWebVitals"; // Importa la función para reportar métricas de rendimiento
 
 // Renderiza el componente App en el elemento con id 'root'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
